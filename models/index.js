4f8aac119a7017203c05d2e26c20b624152858c1
notalisk <81662512+notalisk@users.noreply.github.com>
2023-05-25 11:12:11 -0500

    fixed foreign key constraint bug

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'id'
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
